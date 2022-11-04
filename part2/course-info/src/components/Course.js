f26e05282bddb40dbf12ac1b23bcac1d6120bce7
Howard Wang <howard122696@gmail.com>
2022-11-04 00:13:04 -0700

    2.2 2.3

--- part2/course-info/src/components/Course.js
+++ part2/course-info/src/components/Course.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Part from './Part'
 
 const Course = ({course}) => {
+  const total = course.parts.reduce((a, c) => a + c.exercises, 0)
+
   return (
     <div>
       <h1>{course.name}</h1>
-      {course.parts.map(part => <Part key={part.id} part={part}/>)}
-      
+      {course.parts.map(part => 
+        <Part key={part.id} part={part}/>
+      )}  
+      <h3>total of {total} exercises</h3>
     </div>
   )
 }
