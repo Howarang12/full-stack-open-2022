a06ae84cf2141ac9b9c7e9d015c3ddd2f7a003af
Howard Wang <howard122696@gmail.com>
2022-11-05 21:56:31 -0700

    2.9 phonebook step 4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,12 +3,14 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '123-456-7890'
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [number, setNumber] = useState('')
+  const [filter, setFilter] = useState('')
   
 
   const handleClick = (e) => {
@@ -16,16 +18,21 @@ const App = () => {
     if(persons.some(person => person.name === newName)){
       alert(`${newName} is already added to the phonebook`)
     } else {
-      setPersons([...persons, {name: newName, number: number}])
+      setPersons([...persons, {name: newName, number: number, id: persons.length + 1}])
       setNewName('')
     }
-    
   }
+
+  const filteredList = filter ? 
+    persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())) 
+    : persons
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <input value={filter} onChange={(e) => setFilter(e.target.value)}/>
       <form>
+        <h3>Add a new person</h3>
         <div>
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
         </div>
@@ -37,8 +44,13 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
-        <Person name={person.name} number={person.number}/>
+      {filteredList.map(person => 
+        <Person 
+          key={person.id} 
+          name={person.name} 
+          number={person.number} 
+          id={person.id}
+        />
       )}
     </div>
   )
--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Person = ({name, number}) => {
+const Person = ({name, number, id}) => {
   return (
     <div>{name} {number}</div>
   )
