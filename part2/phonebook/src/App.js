231be597ae4589c92078099bce301887ae72a5b0
Howard Wang <howard122696@gmail.com>
2022-11-11 00:18:16 -0800

    2.16 phonebook step 8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,24 +3,18 @@ import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import List from './components/List'
+import phonebookService from './services/phonebook'
 
 const App = () => {
-  // const [persons, setPersons] = useState([
-  //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  // ])
-
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => setPersons(response.data))
+    phonebookService
+      .getAll()
+      .then(allPeople => setPersons(allPeople))
   }, [])
 
   const handleFormSubmission = (e) => {
@@ -31,10 +25,10 @@ const App = () => {
 
       const newPerson = {name: newName, number: newNumber}
 
-      axios
-        .post('http://localhost:3001/persons', newPerson)
-        .then(response => {
-          setPersons([...persons, response.data])
+      phonebookService
+        .addPerson(newPerson)
+        .then(newPerson => {
+          setPersons([...persons, newPerson])
           setNewName('')
           setNewNumber('')
         })
