a9ac0d89c397ab9dfd54225edd82b8c21272d5f9
Howard Wang <howard122696@gmail.com>
2022-11-10 17:06:21 -0800

    2.15 phonebook step 7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,27 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
+import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import List from './components/List'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  // ])
+
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
 
   const handleFormSubmission = (e) => {
     e.preventDefault()
     if(persons.some(person => person.name === newName)){
       alert(`${newName} is already added to the phonebook`)
     } else {
-      setPersons([...persons, {name: newName, number: newNumber, id: persons.length + 1}])
-      setNewName('')
-      setNewNumber('')
+
+      const newPerson = {name: newName, number: newNumber}
+
+      axios
+        .post('http://localhost:3001/persons', newPerson)
+        .then(response => {
+          setPersons([...persons, response.data])
+          setNewName('')
+          setNewNumber('')
+        })
+        .catch(err => console.log(err))
     }
   }
 
