f9cee203b80a1176be2dc6aa6ab673c5c68d6bad
Howard Wang <howard122696@gmail.com>
2022-11-15 00:16:03 -0800

    3.2 phonebook backend step 2

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -34,4 +34,11 @@ app.get('/api/persons', (req, res) => {
   res.json(data)
 })
 
+app.get('/info', (req, res) => {
+  res.send(`
+    <p> Phonebook has info for ${data.length} people </p>
+    <p> ${Date(Date.now())} </p>
+  `)
+})
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
