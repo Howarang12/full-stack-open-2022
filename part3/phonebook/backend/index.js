9ec5955b3ecbaf849f8d9b9fb7ebff8ab6ade599
Howard Wang <howard122696@gmail.com>
2022-11-18 01:20:58 -0800

    3.9 - 3.11 phonebook step 9-11

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 // const requestLogger = (request, response, next) => {
 //   console.log('Method:', request.method)
@@ -20,11 +21,15 @@ morgan.token('body', req => {
   return JSON.stringify(req.body)
 })
 
+
 app.use(express.json())
+app.use(express.static('build'))
 // app.use(requestLogger)
 // app.use(unknownEndpoint)
 app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
+
 
 let persons = [
   { 
@@ -66,7 +71,6 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.json({msg: 'no person found'}).status(404).end()
   }
-  
 })
 
 app.get('/info', (req, res) => {
