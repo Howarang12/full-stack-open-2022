9e7dfd257c9caca5804eca3970c494f00a709e3d
Howard Wang <howard122696@gmail.com>
2022-11-22 23:53:08 -0800

    notes part 3

--- part3/notes/backend/index.js
+++ part3/notes/backend/index.js
@@ -3,10 +3,22 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const Note = require('./models/note')
+const errorHandler = require('./middleware/errorHandler')
+const unknownEndpoint = require('./middleware/unknownEndpoint')
+// const errorHandler = (error, request, response, next) => {
+//   console.error(error.message)
+//   console.log('error')
+
+//   if (error.name === 'CastError') {
+//     return response.status(400).send({ error: 'malformatted id' })
+//   } 
+
+//   next(error)
+// }
 
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -18,10 +30,19 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then(note => {
-    res.json(note)
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+  .then(note => {
+    if(note){
+      res.json(note)
+    } else {
+      res.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
 
@@ -43,13 +64,28 @@ app.post('/api/notes', (req, res) => {
   })
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  res.status(204).end()
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -3,20 +3,9 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/Person')
 
 const PORT = process.env.PORT || 3001
-
-// const requestLogger = (request, response, next) => {
-//   console.log('Method:', request.method)
-//   console.log('Path:  ', request.path)
-//   console.log('Body:  ', request.body)
-//   console.log('---')
-//   next()
-// }
-
-// const unknownEndpoint = (request, response) => {
-//   response.status(404).send({ error: 'unknown endpoint' })
-// }
 
 morgan.token('body', req => {
   return JSON.stringify(req.body)
@@ -32,46 +21,20 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors())
 
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/', (req, res) => {
   res.send('phonebook')
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if(person) {
+  Person.findById(req.params.id).then(person => {
     res.json(person)
-  } else {
-    res.json({msg: 'no person found'}).status(404).end()
-  }
+  })
 })
 
 app.get('/info', (req, res) => {
@@ -89,24 +52,21 @@ app.delete('/api/persons/:id', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
-  console.log(body)
 
-  if(!body.name || !body.number){
-    return res.status(400).json({err: 'name or number cannot be empty'})
+  if (body.name === undefined || body.number === undefined) {
+    console.log('err')
+    return res.status(400).json({ err: 'fields cannot be empty' })
   }
 
-  if(persons.find(person => person.name === body.name)){
-    return res.status(400).json({err: 'name already exists'})
-  }
+  const newPerson = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-  const person = {
-   name: body.name,
-   number: body.number,
-   id: Math.floor(Math.random() * 10000)
-  }
+  newPerson.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 
-  persons = [...persons, person]
-  res.json(person)
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
